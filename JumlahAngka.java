a3d14b39ba4f018503586b9198b05ad30e9a42ca
a7155 <114379365+a7155@users.noreply.github.com>
2023-06-09 12:02:53 +0700

    Update JumlahAngka.java

--- JumlahAngka.java
+++ JumlahAngka.java
@@ -1,9 +1,9 @@
 
 public class JumlahAngka {
     public static void main(String[] args) {
-        int bilangan = Integer.parseInt(System.console().readLine("Masukkan bilangan: "));
+        int bilangan = Integer.parseInt(System.console().readLine("Masukkan Angka bilangan: "));
         int jumlahAngka = hitungJumlahAngka(bilangan);
-        System.out.println("Jumlah angka dalam bilangan " + bilangan + " adalah " + jumlahAngka);
+        System.out.println("Jumlah Angka Dalam Bilangan " + bilangan + " Adalah " + jumlahAngka);
     }
     
     public static int hitungJumlahAngka(int bilangan) {
